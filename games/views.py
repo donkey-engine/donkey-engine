ae4c4ee8d23011abfaa3cf047157f1e82f0df5e8
Korgoth <shutkadada@yandex.ru>
2021-02-08 04:23:19 +0300

    Add don't work Api Games, I don't know

--- games/views.py
+++ games/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import GameSerializer
+from .models import Game
 
-# Create your views here.
-# here
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- main/urls.py
+++ main/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken import views as drf_auth
+
+from rest_framework import routers
+from games import views
+
+router = routers.DefaultRouter()
+router.register(r'game', views.GameViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', drf_auth.obtain_auth_token),
 ]
+
+urlpatterns += router.urls
