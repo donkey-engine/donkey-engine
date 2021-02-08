1af7f2b8f82f0eca7fef7399d8f7e77ea001370b
Korgoth <shutkadada@yandex.ru>
2021-02-08 22:08:50 +0300

    Add comments in serializers, urls, views.

--- games/views.py
+++ games/views.py
@@ -3,7 +3,9 @@ from rest_framework import permissions
 from .serializers import GameSerializer
 from .models import Game
 
+
 class GameViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows games to be viewed."""
     queryset = Game.objects.all()
     serializer_class = GameSerializer
     permission_classes = [permissions.IsAuthenticated]
