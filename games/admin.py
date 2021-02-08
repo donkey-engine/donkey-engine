a0d8aa5c08e2c9f80396c53f4176975619cfcf12
Korgoth <shutkadada@yandex.ru>
2021-02-08 23:45:02 +0300

    Change name games in admin panel.

--- games/admin.py
+++ games/admin.py
@@ -3,6 +3,11 @@ from .models import Game
 from servers.models import Server
 
 
+class GmaeAdmin(admin.ModelAdmin):
+    """Change name game in admin panel"""
+    list_display = ('name',)
+
+
 # Models for admin panel
-admin.site.register(Game)
+admin.site.register(Game, GmaeAdmin)
 admin.site.register(Server)
