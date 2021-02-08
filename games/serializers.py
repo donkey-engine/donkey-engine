b48e948f50d4b94ae08406ac1a81c25f9159ce92
Korgoth <shutkadada@yandex.ru>
2021-02-08 22:08:50 +0300

    Add comments in serializers, urls, views.

--- games/serializers.py
+++ games/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Game
 
+
 class GameSerializer(serializers.ModelSerializer):
+    """Serializer for Game"""
     class Meta:
         model = Game
         fields = ['id', 'name']
--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from rest_framework.authtoken import views as drf_auth
 
 from rest_framework import routers
@@ -22,6 +22,7 @@ from games import views
 
 router = routers.DefaultRouter()
 router.register(r'game', views.GameViewSet)
+# Linking the API via automatic routing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
