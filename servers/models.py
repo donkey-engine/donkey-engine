65642a149024fa7e9041c7dd696e5db2ad2ad49f
Korgoth <shutkadada@yandex.ru>
2021-02-09 01:27:03 +0300

    Fix names.

--- servers/models.py
+++ servers/models.py
@@ -7,3 +7,6 @@ from games.models import Game
 class Server(models.Model):
     game = models.ForeignKey(Game, on_delete=models.PROTECT, null=False, blank=False)
     admin = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}:{self:game}'
