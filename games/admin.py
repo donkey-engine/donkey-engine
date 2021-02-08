6ddfbb9d3cb8f6e4aef485dee04e0f7d5ec461e8
Korgoth <shutkadada@yandex.ru>
2021-02-09 01:27:03 +0300

    Fix names.

--- games/admin.py
+++ games/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import Game
-from servers.models import Server
 
 
-class GmaeAdmin(admin.ModelAdmin):
+class GameAdmin(admin.ModelAdmin):
     """Change name game in admin panel"""
     list_display = ('name',)
 
 
 # Models for admin panel
-admin.site.register(Game, GmaeAdmin)
-admin.site.register(Server)
+admin.site.register(Game, GameAdmin)
--- games/models.py
+++ games/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Game(models.Model):
     name = models.CharField(max_length=64, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
