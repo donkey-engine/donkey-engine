03a5cebb1462734711e152dbd965f4a6032d258b
Korgoth <shutkadada@yandex.ru>
2021-02-13 22:26:00 +0300

    Fix registration api finished.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class SignupSerializer(serializers.Serializer):
     """Serializer for Signup"""
     email = serializers.EmailField()
-    username = serializers.CharField(max_length=200)
-    password = serializers.DateTimeField()
+    username = serializers.CharField()
+    password = serializers.CharField()
