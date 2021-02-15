f661b75be5f391555f1d2c30b2299dbed12d5df9
Korgoth <shutkadada@yandex.ru>
2021-02-15 16:52:41 +0300

    Create models version and mods, add to admin panel.

--- games/serializers.py
+++ games/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
 from games.models import Game
+from games.models import Mods
 
 
 class GameSerializer(serializers.ModelSerializer):
     """Serializer for Game"""
     class Meta:
         model = Game
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'version']
+
+
+class ModsSerializer(serializers.ModelSerializer):
+    """Serializer for Mods"""
+    class Meta:
+        model = Mods
+        fields = ['id', 'name', 'game', 'version']
--- games/views.py
+++ games/views.py
@@ -1,11 +1,20 @@
 from rest_framework import permissions, viewsets
 
 from games.models import Game
+from games.models import Mods
 from games.serializers import GameSerializer
+from games.serializers import ModsSerializer
 
 
 class GameViewSet(viewsets.ModelViewSet):
     """API endpoint that allows games to be viewed."""
     queryset = Game.objects.all()
     serializer_class = GameSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ModsViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows games to be viewed."""
+    queryset = Mods.objects.all()
+    serializer_class = ModsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
