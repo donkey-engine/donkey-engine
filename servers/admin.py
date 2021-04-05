51c2bd9e2284736a75522764c58811e1ac8a97d5
behindloader <simonsuprun@gmail.com>
2021-04-05 23:02:34 +0300

    Expand models

--- servers/admin.py
+++ servers/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 
-from servers.models import Server
+from servers.models import Server, ServerBuild
 
 
 class ServerAdmin(admin.ModelAdmin):  # type: ignore
-    list_display = ('owner', 'game')
-    list_filter = ('owner', 'game')
+    list_display = ('owner', 'game', 'status')
+    list_filter = ('owner', 'game', 'status')
+
+
+class ServerBuildAdmin(admin.ModelAdmin):  # type: ignore
+    list_display = ('server', 'status', 'started', 'finished')
+    list_filter = ('server', 'status')
 
 
 admin.site.register(Server, ServerAdmin)
+admin.site.register(ServerBuild, ServerBuildAdmin)
