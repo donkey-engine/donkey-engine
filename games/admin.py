56fc609cb39b14c2686720e8f0c39907ae524287
behindloader <simonsuprun@gmail.com>
2021-04-05 16:57:12 +0300

    Init typings

--- games/admin.py
+++ games/admin.py
@@ -3,17 +3,17 @@ from django.contrib import admin
 from games.models import Game, Mods, Version
 
 
-class GameAdmin(admin.ModelAdmin):
+class GameAdmin(admin.ModelAdmin):  # type: ignore
     """Change name game in admin panel"""
     list_display = ('name',)
 
 
-class VersionAdmin(admin.ModelAdmin):
+class VersionAdmin(admin.ModelAdmin):  # type: ignore
     """Change name version in admin panel"""
     list_display = ('version', 'game',)
 
 
-class ModsAdmin(admin.ModelAdmin):
+class ModsAdmin(admin.ModelAdmin):  # type: ignore
     """Change name mods in admin panel"""
     list_display = ('name', 'game', 'version',)
 
