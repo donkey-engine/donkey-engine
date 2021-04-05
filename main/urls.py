52ca2ebbb690817df9e99ef5ce87ddb273c10dd0
behindloader <simonsuprun@gmail.com>
2021-04-05 19:32:30 +0300

    Init swagger

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import routers
 from rest_framework.authtoken import views as drf_auth
 
@@ -16,5 +19,20 @@ urlpatterns = [
     path('auth/', drf_auth.obtain_auth_token),
     path('signup/', account_view.SignupApiView.as_view()),
 ]
+
+if settings.DEBUG:
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Donkey Engine API",
+            default_version='v1',
+            contact=openapi.Contact(email="hello@donkey-engine.host"),
+        ),
+        public=True,
+    )
+
+    urlpatterns += [
+        path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    ]
 
 urlpatterns += router.urls
--- settings/base.py
+++ settings/base.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
 
+    'drf_yasg',
+
     'games',
     'servers',
     'accounts',
