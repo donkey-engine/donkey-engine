69a4d10526a57c9f0f2803b24ab962363c0b927c
behindloader <simonsuprun@gmail.com>
2021-04-26 20:40:16 +0300

    Add auth route

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
 
+class AuthSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
 class SignupSerializer(serializers.Serializer):
     """Serializer for Signup"""
     email = serializers.EmailField()
