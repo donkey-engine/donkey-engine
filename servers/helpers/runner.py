3da44669e60c6041e192f0e70507126b4d19fbad
behindloader <simonsuprun@gmail.com>
2021-04-27 21:35:29 +0300

    Add port choosing

--- servers/helpers/runner.py
+++ servers/helpers/runner.py
@@ -17,6 +17,17 @@ class GameRunner:
             ).absolute()
         )
 
+    def get_container_port(self, attempts=100) -> int:
+        for _ in range(attempts):
+            for container in self.client.containers.list():
+                if container.name != f'server{self.server_id}':
+                    continue
+                for port in container.ports.get('25565/tcp', []):
+                    if not port['HostPort']:
+                        continue
+                    return int(port['HostPort'])
+        return 0
+
     def get_or_create_image(self) -> str:  # TODO add reuse existed images
         image, build_log = self.client.images.build(
             tag=f'server_build_{self.server_id}',
@@ -26,7 +37,14 @@ class GameRunner:
         )
         return image
 
-    def run(self) -> None:
+    def stop(self) -> None:
+        for container in self.client.containers.list():
+            if container.name == f'server{self.server_id}':
+                container.stop()
+                break
+
+    def run(self) -> int:
+        self.stop()
         image_name = self.get_or_create_image()
         self.client.containers.run(
             image_name,
@@ -35,12 +53,21 @@ class GameRunner:
                 f'{self.dockerfile_path}:/home/app',
             ],
             ports={
-                '25565/tcp': 25565,
+                '25565/tcp': None,
             },
             name=f'server{self.server_id}',
+            remove=True,
+            hostname=f'server{self.server_id}',
+            mem_limit='1024M',
         )
+        return self.get_container_port()
 
 
 def run_server(server_id: int) -> None:
     runner = GameRunner(server_id)
     runner.run()
+
+
+def stop_server(server_id: int) -> None:
+    runner = GameRunner(server_id)
+    runner.stop()
