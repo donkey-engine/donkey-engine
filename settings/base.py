a0300bee6d219500ca5b8bdda5c51adfd7fcd004
behindloader <simonsuprun@gmail.com>
2021-05-12 17:54:57 +0300

    Update allowed hosts

--- settings/base.py
+++ settings/base.py
@@ -5,11 +5,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
+    'web',
 ]
 
 INSTALLED_APPS = [
