fe497903aba493509e5537ce4840826e76430b31
tmcgroul <safonov.a.r@mail.ru>
2021-05-15 01:19:29 +0300

    move from api/versions to api/games/<id>/versions

--- games/views.py
+++ games/views.py
@@ -1,14 +1,27 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from games.models import Game, GameVersion, Mod
-from games.serializers import (GameSerializer, GameVersionSerializer,
-                               ModSerializer)
+from games.models import Game, Mod
+from games.serializers import GameSerializer, GameVersionSerializer, ModSerializer
 
 
 class GameViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint that allows games to be viewed."""
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+
+    @action(detail=True)
+    def versions(self, request, pk):
+        game = self.get_object()
+        queryset = game.gameversion_set.all()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = GameVersionSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = GameVersionSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class GameModViewSet(viewsets.ReadOnlyModelViewSet):
@@ -17,11 +30,3 @@ class GameModViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         gameid = self.kwargs.get('game_id')
         return Mod.objects.filter(game_id=gameid)
-
-
-class GameVersionViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = GameVersionSerializer
-
-    def get_queryset(self):
-        game_id = self.request.query_params.get('game_id')
-        return GameVersion.objects.filter(game_id=game_id)
