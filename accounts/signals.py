df112608dc2b3f6f162c98847c71de17ce2746ed
behindloader <simonsuprun@gmail.com>
2021-05-28 20:26:24 +0300

    Add ingore SMTP errors

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from smtplib import SMTPException
 
 from accounts.helpers.email import send_email_confirmation
 
@@ -8,4 +9,8 @@ from accounts.helpers.email import send_email_confirmation
 @receiver(post_save, sender=User)
 def handle_user_post_save(sender, instance, created, **kwargs):
     if created:
-        send_email_confirmation(instance)
+        try:
+            send_email_confirmation(instance)
+        except SMTPException:
+            instance.is_active = True
+            instance.save()
