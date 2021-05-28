0ff1245534fdbcb84410e68a87915f7f26767f01
behindloader <simonsuprun@gmail.com>
2021-05-28 20:29:40 +0300

    Fix imports

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,7 +1,8 @@
+from smtplib import SMTPException
+
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from smtplib import SMTPException
 
 from accounts.helpers.email import send_email_confirmation
 
