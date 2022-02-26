559dfce0cc684b050b120e1f89255b56d4114b73
BehindLoader <hello@syoma.website>
2022-02-26 22:48:43 +0300

    Add config for redis

--- settings/base.py
+++ settings/base.py
@@ -131,8 +131,8 @@ EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL') == 'True'
 HOST_NAME = 'https://donkey-engine.host'
 LOGIN_PAGE = 'https://donkey-engine.host/login'
 
-REDIS_HOST = 'redis'
-REDIS_PORT = 6379
+REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
+REDIS_PORT = int(os.getenv('REDIS_PORT', '6379'))
 
 SERVICE_WSBACKEND_HOST = os.getenv('SERVICE_WSBACKEND_HOST', 'wsbackend')
 SERVICE_WSBACKEND_PORT = int(os.getenv('SERVICE_WSBACKEND_PORT', '8888'))
